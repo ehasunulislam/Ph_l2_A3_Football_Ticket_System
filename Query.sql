c91f8a63da0fba1292d7264d1f1443f54341a0fa
ehasun15019 <ehasunulislam0202@gmail.com>
2026-06-14 00:05:02 +0600

    solve the quesiton 1

--- Query.sql
+++ Query.sql
@@ -56,3 +56,11 @@ INSERT INTO Bookings (booking_id, user_id, match_id, seat_number, payment_status
 (503, 2, 101, 'A-13', 'Confirmed', 150.00),
 (504, 2, 101, NULL, NULL, 150.00),
 (505, 3, 102, 'C-20', 'Pending', 120.00);
+
+
+-- =========================
+-- SOLVE THE QUESTION
+-- =========================
+
+-- Query 1: Retrieve all upcoming football matches belonging to the 'Champions League' where the match status is 'Available'.
+SELECT * FROM matches WHERE match_status = 'Available' AND tournament_category = 'Champions League';
